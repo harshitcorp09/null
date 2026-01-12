d178305f30c46938133310a9269ae1544dfb3005
harshitcorp09 <162092528+harshitcorp09@users.noreply.github.com>
2026-01-12 16:31:27 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,4 +2,5 @@ resource "null_resource" "null-custom" { }
 resource "null_resource" "null-custom2" { }
 resource "null_resource" "null-custom3" { }
 resource "null_resource" "null-custom4" { }
+resource "null_resource" "null-custom5" { }
 
